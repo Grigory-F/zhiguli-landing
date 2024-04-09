a779897a0368a27026a0ff3aef5926a2b9dd5c44
Grigory-F <grishka2500@gmail.com>
2024-04-09 20:31:53 +0400

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -5,58 +5,72 @@ import {
   addTouchClass,
   addLoadedClass,
   menuInit,
-} from "./modules";
+} from './modules'
 
+import { Modal } from 'bootstrap'
+import Swiper, {
+  Navigation,
+  Pagination,
+  Thumbs,
+  Controller,
+  Autoplay,
+} from 'swiper'
 
-import { Modal } from "bootstrap";
+let listNames = [
+  'Главное',
+  'Транспорт',
+  'Клиенты',
+  'Безопасность',
+  'Сотрудники',
+]
 
-let headerNav = document.querySelector('.header-nav')
-let listLinkNav = document.querySelectorAll('.header-nav ul a')
-let headerBurger = document.querySelector('.header-burger')
-let portfolioBlockEnterBtn = document.querySelector('.portfolio-block__enter-btn img')
-let formModalSubmitBtn = document.querySelector('.form-modal__submit-btn')
-let formModalInput = document.querySelectorAll('.form-modal__input[type=text], textarea.form-modal__input')
-let overflowMenu = document.querySelector('.header-nav__overflow')
-
-
-formModalSubmitBtn.addEventListener('click', () => {
-  formModalInput.forEach((elem, index) => {
-    elem.value = ''
-    elem.textContent = ''
-  })
+let mainSlider = new Swiper('.main-slider', {
+  modules: [Navigation, Pagination, Autoplay],
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    delay: 10000,
+  },
+  navigation: {
+    prevEl: '.prev-btn',
+    nextEl: '.next-btn',
+  },
+  pagination: {
+    el: '.custom-pagination__ul',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<li class="' + className + '">' + listNames[index] + '</li>'
+    },
+  },
 })
 
+let headerBurger = document.querySelector('.header-burger')
+let hiddenNav = document.querySelector('.hidden-nav')
 
 headerBurger.addEventListener('click', (e) => {
   headerBurger.classList.toggle('header-burger--open')
-  headerNav.classList.toggle('header-nav--open')
-  overflowMenu.classList.toggle('header-nav__overflow--open')
+  hiddenNav.classList.toggle('hidden-nav--open')
 })
 
+// listLinkNav.forEach((element, index) => {
+//   element.addEventListener('click', () => {
+//     headerBurger.classList.remove('header-burger--open')
+//     headerNav.classList.remove('header-nav--open')
+//     overflowMenu.classList.remove('header-nav__overflow--open')
+//   })
+// })
 
+// overflowMenu.addEventListener('click', (e) => {
+//   overflowMenu.classList.remove('header-nav__overflow--open')
+//   headerBurger.classList.remove('header-burger--open')
+//   headerNav.classList.remove('header-nav--open')
+// })
 
-listLinkNav.forEach((element, index) => {
-  element.addEventListener('click', () => {
-    headerBurger.classList.remove('header-burger--open')
-    headerNav.classList.remove('header-nav--open')
-    overflowMenu.classList.remove('header-nav__overflow--open')
-  })
-});
+// portfolioBlockEnterBtn.addEventListener('mousedown', () => {
+//   portfolioBlockEnterBtn.src = './images/button-down.png'
+// })
+// portfolioBlockEnterBtn.addEventListener('mouseup', () => {
+//   portfolioBlockEnterBtn.src = './images/button.png'
+// })
 
-overflowMenu.addEventListener('click', (e) => {
-  overflowMenu.classList.remove('header-nav__overflow--open')
-  headerBurger.classList.remove('header-burger--open')
-    headerNav.classList.remove('header-nav--open')
-})
-
-
-
-portfolioBlockEnterBtn.addEventListener("mousedown", () => {
-  portfolioBlockEnterBtn.src = './images/button-down.png' 
-});
-portfolioBlockEnterBtn.addEventListener("mouseup", () => {
-  portfolioBlockEnterBtn.src = './images/button.png'
-});
-
-
-window["FLS"] = location.hostname === "localhost";
+window['FLS'] = location.hostname === 'localhost'
